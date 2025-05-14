52335829d74c82667c15bd866843f6331824fd32
acer51-doctom <110639772+acer51-doctom@users.noreply.github.com>
2025-05-14 20:22:40 +0200

    modified src/sd_path_prompt.py

--- src/sd_path_prompt.py
+++ src/sd_path_prompt.py
@@ -1,4 +1,11 @@
 # Prompts user for SD Card path
+
+import os
+
 sd_card_path = input("What is the path of your SD Card? ") # space is intended
 
-print(f"You entered: {sd_card_path}") # temporary until further notice
+if os.path.exists(sd_card_path):
+    print("SD Card volume exists.")
+
+else:
+    print("Volume does not exist. Maybe wrong input?")
