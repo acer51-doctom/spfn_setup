dabb0b6eaf6f081cc358ff9964108d949d5034de
acer51-doctom <110639772+acer51-doctom@users.noreply.github.com>
2025-05-14 20:19:35 +0200

    modified src/sd_path_prompt.py

--- src/sd_path_prompt.py
+++ src/sd_path_prompt.py
@@ -1,1 +1,4 @@
 # Prompts user for SD Card path
+sd_card_path = input("What is the path of your SD Card? ") # space is intended
+
+print(f"You entered: {sd_card_path}") # temporary until further notice
